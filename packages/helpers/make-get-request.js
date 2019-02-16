f44ba4601426558697ae78b1f2410b1a25acbb1d
Ed Kelly <ed@newlinelabs.com>
2019-02-16 18:22:33 +0000

    Circleci

--- packages/helpers/make-get-request.js
+++ packages/helpers/make-get-request.js
@@ -1,13 +1,11 @@
 const request = require('request-promise');
 const { TESCO_API_KEY } = process.env;
 
-module.exports = async (path, qs) => {
-  return request({
-    method: 'GET',
-    uri: `https://dev.tescolabs.com${path}`,
-    qs,
-    headers: {
-      'Ocp-Apim-Subscription-Key': TESCO_API_KEY
-    }
-  });
-};
+module.exports = (path, qs) => request({
+  method: 'GET',
+  uri: `https://dev.tescolabs.com${path}`,
+  qs,
+  headers: {
+    'Ocp-Apim-Subscription-Key': TESCO_API_KEY
+  }
+});
