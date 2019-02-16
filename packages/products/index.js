67ffae224bb093dc70c58d6b34ff46738f7f0c03
Ed Kelly <ed@newlinelabs.com>
2019-02-16 18:27:29 +0000

    Circleci

--- packages/products/index.js
+++ packages/products/index.js
@@ -3,12 +3,21 @@ const { assert } = require('chai');
 const getProducts = require('./helpers/get-products');
 
 module.exports = (req, res) => {
-  assert(req.query.query !== undefined, '"name" query parameter is required');
-  assert(req.query.offset !== undefined, '"offset" query parameter is required');
+  try {
+    assert(req.query.query !== undefined, '"name" query parameter is required');
+    assert(req.query.offset !== undefined, '"offset" query parameter is required');
+  } catch (err) {
+    res.status(400)
+      .send(err);
+  }
 
   getProducts({ query: req.query.query, offset: req.query.offset })
-    .then((products) => () => {
+    .then((products) => {
       res.status(200)
         .send(products);
+    })
+    .catch((err) => {
+      res.status(500)
+        .send(err);
     });
 };
