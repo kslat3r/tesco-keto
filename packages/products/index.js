a2ffee5b29149ec06d06bf727119618836e219b6
Ed Kelly <ed@newlinelabs.com>
2019-02-16 19:30:06 +0000

    Circleci

--- packages/products/index.js
+++ packages/products/index.js
@@ -8,19 +8,18 @@ module.exports = (req, res) => {
     assert(req.query.offset !== undefined, '"offset" query parameter is required');
     assert(req.query.limit !== undefined, '"limit" query parameter is required');
   } catch (err) {
-    res.status(400)
+    return res.status(400)
       .send({ error: err.message });
-
-    return;
   }
 
-  getProducts({ query: req.query.query, offset: req.query.offset, limit: req.query.limit })
+  return getProducts({ query: req.query.query, offset: req.query.offset, limit: req.query.limit })
     .then((result) => {
-      // const products = result.uk.ghs.products.results;
-      // const tpnbs = products.map(product => product.tpnb);
+      console.log(result);
+      const products = result.uk.ghs.products.results;
+      const tpnbs = products.map(product => product.tpnb);
 
       res.status(200)
-        .send(result);
+        .send(tpnbs);
     })
     .catch((err) => {
       res.status(500)
--- packages/products/index.spec.js
+++ packages/products/index.spec.js
@@ -1,7 +1,18 @@
 const { expect } = require('chai');
+const products = require('./');
 
 describe('products', () => {
-  it('runs tests', () => {
-    expect(true).to.equal(true);
+  const req = { query: 'bread', offset: 0, limit: 100 };
+  const res = {
+    status: () => ({
+      send: () => {}
+    })
+  };
+
+  it('gets results', () => {
+    products(req, res)
+      .then((result) => {
+        expect(result.length > 0).to.be.true();
+      });
   });
 });
