281fcf4227360a59505ff41ea301af846d7c354b
Ed Kelly <ed@newlinelabs.com>
2019-02-18 23:12:33 +0000

    Circleci

--- packages/homepage/index.js
+++ packages/homepage/index.js
@@ -1,6 +1,6 @@
 const next = require('next');
 const wrapWebApp = require('../helpers/wrap-web-app');
 
-const app = next({ dev: false });
+const app = next({ dir: './', dev: false });
 
 module.exports = wrapWebApp(app);
