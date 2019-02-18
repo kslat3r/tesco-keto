1c5388c725f26cc7d28b6d4e115812b1c97e5417
Ed Kelly <ed@newlinelabs.com>
2019-02-18 23:37:38 +0000

    Circleci

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const homepage = require('./packages/homepage');
 const products = require('./packages/products');
 
+console.log(homepage);
+
 module.exports.homepage = homepage;
 module.exports.products = products;
-
--- packages/helpers/wrap-web-app.js
+++ packages/helpers/wrap-web-app.js
@@ -1,24 +1,23 @@
-const { createServer } = require('http');
 const { parse } = require('url');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 
-module.exports = (app) => {
+module.exports = (app) => (req, res) => {
   const handle = app.getRequestHandler();
 
   return app.prepare()
     .then(() => {
-      createServer((req, res) => {
-        const parsedUrl = parse(req.url, true);
-        const { pathname, query } = parsedUrl;
+      console.log(req);
 
-        if (pathname === '/') {
-          app.render(req, res, '/', query);
-        } else {
-          handle(req, res, parsedUrl);
-        }
-      }).listen(port, err => {
-        if (err) throw err;
-      });
+      const parsedUrl = parse(req.url, true);
+      const { pathname, query } = parsedUrl;
+
+      if (pathname === '/') {
+        app.render(req, res, '/', query);
+      } else {
+        handle(req, res, parsedUrl);
+      }
+    }).listen(port, err => {
+      if (err) throw err;
     });
 };
