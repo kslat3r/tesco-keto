03e9ad307713ae112564356c6eca1402cdc02a91
Ed Kelly <ed@newlinelabs.com>
2019-02-18 21:27:29 +0000

    Circleci

--- index.js
+++ index.js
@@ -2,7 +2,37 @@ const homepage = require('./packages/homepage/.next/serverless/pages');
 const products = require('./packages/products');
 
 const wrap = (page) => (req, res) => {
-  page.render(req, res);
+  const express = require("express");
+  const next = require("next");
+
+  const port = parseInt(process.env.PORT, 10) || 3000;
+  const dev = process.env.NODE_ENV !== "production";
+  const app = next({ dev });
+  const handle = app.getRequestHandler();
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+
+    server.get("/", (req, res) => {
+      return app.render(req, res, "/", req.params);
+    });
+
+    server.get("*", (req, res) => {
+      return handle(req, res);
+    });
+
+    server.listen(port, err => {
+      if (err) throw err;
+
+      console.log(`> Ready on http://localhost:${port}`);
+    });
+  })
+  .catch(ex => {
+    console.log(ex);
+    process.exit(1);
+  });
 };
 
 module.exports.homepage = wrap(homepage);
